ed72da4ff4f33965374c670177583aa4d11bd99c
nixigg86 <dev.py86@gmail.com>
2020-11-22 20:59:03 -0800

    Added Global Style Component. About component was styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+// Import global style
+import GlobalStyle from './components/GlobalStyle';
 // Import Pages
 import AboutUs from './pages/AboutUs';
+
+
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
